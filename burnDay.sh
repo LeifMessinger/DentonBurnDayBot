d5b9ddb11b6b4a6f23f8785817add8d5188322ab
Leif Messinger <leifmessinger@gmail.com>
2023-02-26 00:30:38 -0600

    Initial commit before testing

---
+++ burnDay.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+url="https://www.dentoncountyesd1.gov/wp-content/uploads/scripts/burnday.php"
+#TODO: Use the "It's a burn day" string instead, so no false positives. Have to wait for a burn day to make sure that's the right text.
+not_burn_day_string="NOT a burn day"
+#This is a file where your webhook endpoint URL is
+#The file might not be allowed to have a newline in it, so use printf "<URL>" >> webhookURL.txt
+post_url=$(cat ./webhookURL.txt)
+
+# Set a default path if $1 is not defined
+json_path=${1:-TisBurnDay.json}
+
+#Request the html from the url
+response=$(curl -s "$url")
+# If the string is not found in the html
+if [[ $response != *"$not_burn_day_string"* ]]; then
+	echo "It's a burn day!"
+	date
+	# Send a POST request with the JSON file
+	curl -X POST -H "Content-Type: application/json" -d "@$json_path" "$post_url"
+else
+	echo "Not a burn day. No message sent."
+fi
