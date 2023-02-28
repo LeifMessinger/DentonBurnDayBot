390ae9f064c386d44b27cda59e9a1e51f5a59bc7
Leif Messinger <leifmessinger@gmail.com>
2023-02-28 00:59:43 -0600

    Did some testing, added pinging.
    Still need to wait for a burn day to put in the correct burn day string.
    That will solve false positives.

---
+++ burnDayRegardless.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+url="https://www.dentoncountyesd1.gov/wp-content/uploads/scripts/burnday.php"
+#TODO: Use the "It's a burn day" string instead, so no false positives. Have to wait for a burn day to make sure that's the right text.
+not_burn_day_string="NOT a burn day"
+#This is a file where your webhook endpoint URL is
+#The file might not be allowed to have a newline in it, so use printf "<URL>" >> webhookURL.txt
+post_url=$(cat ./webhookURL.txt)
+
+# Set a default path if $1 is not defined
+tisBurnDayJSON=${1:-TisBurnDay.json}
+
+# Set a default path if $1 is not defined
+notBurnDayJSON=${1:-NotBurnDay.json}
+
+#Request the html from the url
+response=$(curl -s "$url")
+# If the string is not found in the html
+if [[ $response != *"$not_burn_day_string"* ]]; then
+	echo "It's a burn day!"
+	date
+	# Send a POST request with the JSON file
+	curl -X POST -H "Content-Type: application/json" -d "@$tisBurnDayJSON" "$post_url"
+else
+	echo "It's not a burn day. :("
+	date
+	# Send a POST request with the JSON file
+	curl -X POST -H "Content-Type: application/json" -d "@$notBurnDayJSON" "$post_url"
+fi
