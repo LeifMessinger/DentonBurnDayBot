06a2bc7f2e9d9524b7455582dfe7e986df893103
Leif Messinger <leifmessinger@gmail.com>
2023-02-28 10:42:02 -0600

    v0.95, need to test crontab because it didn't go off

--- burnDayRegardless.sh
+++ burnDayRegardless.sh
@@ -2,7 +2,7 @@
 
 url="https://www.dentoncountyesd1.gov/wp-content/uploads/scripts/burnday.php"
 #TODO: Use the "It's a burn day" string instead, so no false positives. Have to wait for a burn day to make sure that's the right text.
-not_burn_day_string="NOT a burn day"
+burn_day_string="is a burn day"
 #This is a file where your webhook endpoint URL is
 #The file might not be allowed to have a newline in it, so use printf "<URL>" >> webhookURL.txt
 post_url=$(cat ./webhookURL.txt)
@@ -16,7 +16,7 @@ notBurnDayJSON=${1:-NotBurnDay.json}
 #Request the html from the url
 response=$(curl -s "$url")
 # If the string is not found in the html
-if [[ $response != *"$not_burn_day_string"* ]]; then
+if [[ $response == *"$burn_day_string"* ]]; then
 	echo "It's a burn day!"
 	date
 	# Send a POST request with the JSON file
