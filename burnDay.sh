aebb5b832d97ea46a96e3c58e5a002e4859cfbb4
Leif Messinger <leifmessinger@gmail.com>
2023-10-29 01:58:05 -0500

    They cut me off! >:( Now I'm forced to scrape.

--- burnDay.sh
+++ burnDay.sh
@@ -2,20 +2,29 @@
 
 pushd ~/Documents/DentonBurnDayBot/
 
-url="https://www.dentoncountyesd1.gov/wp-content/uploads/scripts/burnday.php"
+#This link no longer works :(
+#url="https://www.dentoncountyesd1.gov/wp-content/uploads/scripts/burnday.php"
+url="https://apps.dentoncounty.gov/website/publicburncontrol/Default"
+
 #TODO: Use the "It's a burn day" string instead, so no false positives. Have to wait for a burn day to make sure that's the right text.
-burn_day_string="is a burn day"
+#burn_day_string="is a burn day"
+#New strategy
+burn_day_string="Images/BurndayYes2.jpg"
+
 #This is a file where your webhook endpoint URL is
 #The file might not be allowed to have a newline in it, so use printf "<URL>" >> webhookURL.txt
 post_url=$(cat ./webhookURL.txt)
 
 # Set a default path if $1 is not defined
-json_path=${1:-TisBurnDay.json}
+tisBurnDayJSON=${1:-TisBurnDay.json}
+
+# Set a default path if $1 is not defined
+notBurnDayJSON=${1:-NotBurnDay.json}
 
 #Request the html from the url
-response=$(curl -s "$url")
+curl -s "$url" > /tmp/burnDayScrape.txt
 # If the string is not found in the html
-if [[ $response == *"$burn_day_string"* ]]; then
+if grep "$burn_day_string" /tmp/burnDayScrape.txt; then
 	echo "It's a burn day!"
 	date
 	# Send a POST request with the JSON file
--- burnDayRegardless.sh
+++ burnDayRegardless.sh
@@ -2,9 +2,16 @@
 
 pushd ~/Documents/DentonBurnDayBot/
 
-url="https://www.dentoncountyesd1.gov/wp-content/uploads/scripts/burnday.php"
+#This link no longer works :(
+#url="https://www.dentoncountyesd1.gov/wp-content/uploads/scripts/burnday.php"
+url="https://apps.dentoncounty.gov/website/publicburncontrol/Default"
+
 #TODO: Use the "It's a burn day" string instead, so no false positives. Have to wait for a burn day to make sure that's the right text.
-burn_day_string="is a burn day"
+#burn_day_string="is a burn day"
+#New strategy
+#TODO: Use the "It's a burn day" string instead, so no false positives. Have to wait for a burn day to make sure that's the right text.
+burn_day_string="Images/BurndayYes2.jpg"
+
 #This is a file where your webhook endpoint URL is
 #The file might not be allowed to have a newline in it, so use printf "<URL>" >> webhookURL.txt
 post_url=$(cat ./webhookURL.txt)
@@ -16,9 +23,9 @@ tisBurnDayJSON=${1:-TisBurnDay.json}
 notBurnDayJSON=${1:-NotBurnDay.json}
 
 #Request the html from the url
-response=$(curl -s "$url")
+curl -s "$url" > /tmp/burnDayScrape.txt
 # If the string is not found in the html
-if [[ $response == *"$burn_day_string"* ]]; then
+if grep "$burn_day_string" /tmp/burnDayScrape.txt; then
 	echo "It's a burn day!"
 	date
 	# Send a POST request with the JSON file
